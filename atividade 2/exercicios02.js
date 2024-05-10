110cd001a05218a55b89e3b7cd4c0734df04dc6d
Iohannes Paulus <126628609+JaumZiiZ@users.noreply.github.com>
2024-05-10 12:09:21 -0300

    Add files via upload

---
+++ atividade 2/exercicios02.js
@@ -0,0 +1,25 @@
+alert("Seja bem-vindo ao nosso site!!");
+
+var nome = "Lua";
+
+var idade = 25; 
+
+var numdevendas = 50;
+
+var saldoDisp = 1000;
+
+alert("Erro! Preencha todos oscampos.")
+
+var msgerro = "ERRO";
+
+alert(msgerro);
+
+nome = prompt("qual é o seu nome?");
+
+idade = prompt("Qual é a sua idade?");
+
+if (idade >= 18) {
+    alert("Pode tirar habilitação!")
+} if (idade < 18) {
+    alert("Não pode tirar habilitação!")
+}
